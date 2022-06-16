24b981dac5f9d50d7d631aceca6de9e8445a3941
Bill Campbell <metobillc@gmail.com>
2022-06-16 10:07:28 -0700

    Clarifications, no substantive change

--- load_truth.m
+++ load_truth.m
@@ -1,18 +1,21 @@
-function [Xt,abstol,reltol] = load_truth(pathname,ftruth)
+function [Zt,abstol,reltol] = load_truth(pathname,ftruth)
+DEFAULT_ABSTOL = 1e-6
+DEFAULT_RELTOL = 1e-3
+
 allvars = load([pathname,ftruth]); % full nature run
-if isfield(allvars, 'Zt') % Zt is correct here, NOT Xt
-    Xt = allvars.Zt;  % See prior comment
-    Xt = Xt';
+if isfield(allvars, 'Zt')
+    Zt = allvars.Zt;
+    Zt = Zt.';
 else
     error('Zt not found in nature run file %s',ftruth);
 end
 if isfield(allvars, 'abstol')
     abstol = allvars.abstol;
 else
-    abstol = 1e-6;
+    abstol = DEFAULT_ABSTOL;
 end
 if isfield(allvars, 'reltol')
     reltol = allvars.reltol;
 else
-    reltol = 1e-3;
+    reltol = DEFAULT_RELTOL;
 end
