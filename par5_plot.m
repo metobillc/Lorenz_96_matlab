090894d5e684ae0bbf886dd75ce95930f4721f64
William Campbell <bill.campbell@nrlmry.navy.mil>
2022-06-21 20:07:08 -0700

    Helper function to load ensemble .mat files, and ensemble plotting for data exploration

--- par5_plot.m
+++ par5_plot.m
@@ -1,34 +1,25 @@
 % This script is for data exploration
 % Bill Campbell
-% Last modified 9/16/2016
+% Last modified 6/21/2022
 
 %% Load prior, posterior, truth, and obs
-outfolder='M:\4_8_2011\NREIP\Summer 2020\Lorenz_96_model_for_interns\';
+outfolder='C:\Users\campbell\Documents\MATLAB\Lorenz_96_model\';
 % prior_alpha_1.10_tf_0.05_R_1.00_loc_n40_nc_50_1st_2_skip_4_seed_29418_GKSQ.mat
-[fprior,pprior]=uigetfile([outfolder,'K1000\prior*'],'Prior');
-[fpost,ppost]=uigetfile([outfolder,'K1000\post*'],'Posterior');
-[ftruth,ptruth]=uigetfile([outfolder,'L*'],'Truth');
+[fprior,pprior]=uigetfile([outfolder,'K500\prior*'],'Prior');
+[fpost,ppost]=uigetfile([outfolder,'K500\post*'],'Posterior');
+[ftruth,ptruth]=uigetfile([outfolder,'*L05*'],'Truth');
 [fobs,pobs]=uigetfile([outfolder,'obs*'],'Obs');
 
-prior=load([pprior,fprior]);
-fnprior=fieldnames(prior);
-prior=prior.(fnprior{1});
-[ngrid,nens,ncycles]=size(prior);
-post=load([ppost,fpost]);
-fnpost=fieldnames(post);
-post=post.(fnpost{1});
-truth=load([ptruth,ftruth]);
-fntruth=fieldnames(truth);
-truth=truth.(fntruth{1});
-truth=truth';
-truth=truth(:,1:ncycles);
-obs=load([pobs,fobs]);
-fnobs=fieldnames(obs);
-obs=obs.(fnobs{1});
-obs=obs(:,1:ncycles);
+prior=load_ensemble(pprior,fprior);  
+[Nx,K,Nt]=size(prior);  % Space x Nmem x Time
+post=load_ensemble(ppost,fpost);
+truth=load_truth(ptruth,ftruth);
+truth=truth(:,1:Nt); % Space x Time
+obs=load_obs(pobs,fobs);
+obs=obs(:,1:Nt); % Space x Time
 % Skip unobserved gridpoints
 string = strsplit(fprior,'_');
-if length(string)>11,
+if length(string)>11
     first = str2num(string{13});
     skip = str2num(string{15});
 else
@@ -36,8 +27,8 @@ else
     skip = 1;
 end
 
-%% Ensemble means, min, and max
-prior_mean=squeeze(mean(prior,2));
+%% Ensemble means, min, and max (Space x Time)
+prior_mean=squeeze(mean(prior,2)); 
 prior_min = squeeze(min(prior,[],2));
 prior_max = squeeze(max(prior,[],2));
 
@@ -47,14 +38,12 @@ post_max = squeeze(max(post,[],2));
 
 %% Forecast and analysis error covariance matrices
 % at the final time
-K = 1000;
-[Nx,tf] = size(prior_mean);
 num=20;
 skm1i=1./sqrt(num)./sqrt(K-1);
 Pfsamp = zeros(Nx);
 Pasamp = Pfsamp;
-for t=tf-num+1:tf,
-    Zt = squeeze(prior(:,:,t));
+for t=Nt-num+1:Nt
+    Zt = squeeze(prior(:,:,t));  % Space x Nmem
     Ztbar = repmat(mean(Zt,2),1,K);
     Zt = skm1i*(Zt - Ztbar);
     Pfsamp = Pfsamp + Zt * Zt';
@@ -64,7 +53,7 @@ for t=tf-num+1:tf,
     Xt = skm1i*(Xt - Xtbar);
     Pasamp = Pasamp + Xt * Xt';
 end
-CL = covloc(40,1,40);
+CL = covloc(Nx,1,Nx/20);
 Pfloc = CL.*Pfsamp;
 Paloc = CL.*Pasamp;
 
