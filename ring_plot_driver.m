7ded2e62cce63999d712cf1107b5d1f35b701195
William Campbell <bill.campbell@nrlmry.navy.mil>
2022-09-03 15:07:53 -0700

    Latest updates with new plot options

--- ring_plot_driver.m
+++ ring_plot_driver.m
@@ -1,6 +1,21 @@
 % This script loads posterior .mat files, then calls 
 % plot_mse_ring.m to produce a ring plots of prior & posterior
 
+%% Plot options
+prompt={'MSE Logscale','MSE Plot Prior', 'Ensvar Logscale', 'Ensvar Plot Prior'};
+name='Ring Plot Options';
+numlines=1;
+default={'0','1','0','1'};,...
+answer=inputdlg(prompt,name,numlines,default);i=1;
+% MSE Logscale
+mse_logscale = str2double(answer{i});i=i+1;
+% MSE Plot Prior
+mse_plot_prior = str2double(answer{i});i=i+1;
+% Ensvar Logscale
+ensvar_logscale = str2double(answer{i});i=i+1;
+% Ensvar Plot Prior
+ensvar_plot_prior = str2double(answer{i});
+
 %% Load posterior, discarding da_spinup
 outfolder = 'C:\Users\campbell\Document\MATLAB\\Lorenz_96\'; % Local hard drive
 [fpost,ppath]=uigetfile([outfolder,'K*/posterior*'],'Choose post file:'); % post
@@ -43,9 +58,9 @@ upper = oneside_ci(scoreKSQ.', ci);
 
 %% Plot MSE, confidence interval, and components of MSE
 if use_errKSQ==1
-    plot_mse_ring(tmse, upper, tbiassq);
+    plot_mse_ring(tmse, upper, mse_logscale, mse_plot_prior, tbiassq);
 else
-    plot_mse_ring(tmse, upper);
+    plot_mse_ring(tmse, upper, mse_logscale, mse_plot_prior);
 end
 
 %% Ensemble variance
@@ -60,7 +75,7 @@ ens_tmse = ens_rmse.^2; % equals ens_tbiassq + ens_tvar
 ens_upper = oneside_ci(ensvarKSQ.', ci);
 
 %% Plot ensemble variance, confidence interval
-plot_ensvar_ring(ens_tmse, ens_upper);
+plot_ensvar_ring(ens_tmse, ens_upper, ensvar_logscale, ensvar_plot_prior);
 
 %% Nature run and observations of the truth
 function [ftruth, truepath, fobs, obspath, spinup] =...
@@ -98,8 +113,8 @@ function upper = oneside_ci(tseries, ci)
     upper = cifac.*stderr;
 end
 
-function plot_mse_ring(mse, upper, varargin)
-    if nargin==3
+function plot_mse_ring(mse, upper, logscale, plot_prior, varargin)
+    if nargin==5
         sqbias = varargin{1};
         var = mse - sqbias;
     end
@@ -110,48 +125,90 @@ function plot_mse_ring(mse, upper, varargin)
     prior = 1:Nx;
     post = Nx+1:2*Nx;
     ring = 1:Nx;
-    plot(ring,mse(prior),'b-',ring,mse(prior)+upper(prior),'b:', ...
-         ring,mse(post),'r-',ring,mse(post)+upper(post),'r:')
+    if plot_prior==1
+        plot(ring,mse(prior),'b-',ring,mse(prior)+upper(prior),'b:');
+    end
     hold on
-    if nargin==3
-        plot(ring,sqbias(prior),'c--',ring,var(prior),'y--',...
-             ring,sqbias(post),'k--',ring,var(post),'m--')
+    plot(ring,mse(post),'r-',ring,mse(post)+upper(post),'r:')
+    if nargin==5
+        if plot_prior==1
+            plot(ring,sqbias(prior),'c-',ring,var(prior),'y-');
+        end
+        plot(ring,sqbias(post),'k-',ring,var(post),'m-')
+        gnir = [ring, fliplr(ring)];
+        if plot_prior==1
+            tween_prior = [var(prior), fliplr(sqbias(prior))];
+            fill(gnir, tween_prior, 'y', 'FaceAlpha', 0.25, 'EdgeAlpha', 0.0);
+        end
+        tween_post = [var(post), fliplr(sqbias(post))];
+        fill(gnir, tween_post, 'm', 'FaceAlpha', 0.25, 'EdgeAlpha', 0.0);
     end
     set(gca,'fontsize',12,'fontweight','bold')
     xlim([1,Nx]);
-    ylim([0.01,Inf]);
-    set(gca,'yscale','log');
+    if logscale==1
+        miny = 0.01;
+        set(gca,'yscale','log');
+        ylim([miny,Inf]);
+    else
+        ylim([0,Inf]);
+    end
     nticks = 15;
     gap = floor(Nx/nticks);
     xticks([1 gap:gap:Nx]);
     grid on
     xlabel('Ring')
     ylabel('Time Mean Squared Error')
-    if nargin==2
-        legend({'Prior','','Post',''},'location','northwest')
-    elseif nargin==3
-        legend({'Prior','','Post','','Prior SqBias','Prior Var',...
-            'Post SqBias','Post Var'},'location','northwest')
+    if nargin==5
+        if plot_prior==1
+            legend({'Prior','','Post','','Prior SqBias','Prior Var',...
+                'Post SqBias','Post Var','',''},'location','northwestoutside');
+        else
+            legend({'Post','','Post SqBias','Post Var',''},...
+                'location','northwestoutside');
+        end
+    else
+        if plot_prior==1
+            legend({'Prior','','Post',''},'location','northwest');
+        else
+            legend({'Post',''},'location','northwest');
+        end
     end
-    ttl1 = sprintf('Posterior Spatial Mean of TMSE: %8.5f',...
-        mean(mse(post)));
-    title({ttl1})
+    smse = mean(mse(post));
+    ssqb = mean(sqbias(post));
+    bpct = round(100 * ssqb / smse, 1);
+    vpct = 100 - bpct;
+    ttl1 = sprintf('Posterior Spatial Mean of TMSE: %8.5f',smse);
+    if nargin==5
+        ttl2 = sprintf('Squared Bias is %4.1f%%, Variance is %4.1f%%',...
+            bpct,vpct);
+        title({ttl1,ttl2});
+    else
+        title({ttl1});
+    end
 end
 
-function plot_ensvar_ring(ensvar, upper)
+function plot_ensvar_ring(ensvar, upper, logscale, plot_prior)
 % ensvar is 1 x 2*Nx
 % First Nx values are the prior; second Nx values are the posterior
     Nx = size(ensvar,2)/2;
-    figure('position',[100 200 1250 500]);
+    figure('position',[300 600 1250 500]);
     prior = 1:Nx;
     post = Nx+1:2*Nx;
     ring = 1:Nx;
-    plot(ring,ensvar(prior),'b-',ring,ensvar(prior)+upper(prior),'b:', ...
-         ring,ensvar(post),'r-',ring,ensvar(post)+upper(post),'r:')
+    if plot_prior==1
+        plot(ring,ensvar(prior),'b-',ring,ensvar(prior)+upper(prior),'b:');
+    end
     hold on
+    plot(ring,ensvar(post),'r-',ring,ensvar(post)+upper(post),'r:')
     set(gca,'fontsize',12,'fontweight','bold')
     xlim([1,Nx]);
-    ylim([0.01,Inf]);
+    if logscale==1
+        miny = 0.01;
+        set(gca,'yscale','log')
+        ylim([miny,Inf]);
+    else
+        ylim([0,Inf]);
+    end
     set(gca,'yscale','log');
     nticks = 15;
     gap = floor(Nx/nticks);
