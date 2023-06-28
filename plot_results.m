adf8d7dca26553301f972c8fd76ced7d0fa7e4e7
William Campbell <bill.campbell@nrlmry.navy.mil>
2023-06-28 08:05:14 -0700

    Remove unneeded code for now

--- plot_results.m
+++ plot_results.m
@@ -1,6 +1,7 @@
 function plot_results(first,current_cycle,prior_mse_norm,prior_varse_norm,...
     post_mse_norm,post_varse_norm,Ncycles,printcycle,ci,K)
-persistent h old_prior_mse_norm old_prior_stderr old_post_mse_norm old_post_stderr cifac
+persistent h old_cycle old_post_mse_norm old_post_stderr cifac
+% persistent old_prior_mse_norm old_prior_stderr
 if first==true
     h = figure;
     xlabel('Cycles');
@@ -26,26 +27,26 @@ Neff = current_cycle * factor;
 % Now compute the standard error as the stdev / sqrt(Neff)
 % This is not quite correct either, as we are dealing with positive
 % definite quantities, which are non-Gaussian close to zero
-prior_stderr = sqrt(prior_varse_norm/Neff);
-prior_upper = prior_mse_norm + cifac*prior_stderr;
+% prior_stderr = sqrt(prior_varse_norm/Neff);
+% prior_upper = prior_mse_norm + cifac*prior_stderr;
 post_stderr = sqrt(post_varse_norm/Neff);
 post_upper = post_mse_norm + cifac*post_stderr;
-da_spinup = 400;
+% da_spinup = 400;
 figure(h);
 hold on
-if current_cycle >= da_spinup
-    plot(current_cycle,prior_mse_norm,'m.',current_cycle,prior_upper,'g');
-end
+% if current_cycle >= da_spinup
+%     plot(current_cycle,prior_mse_norm,'m.',current_cycle,prior_upper,'g:');
+% end
 plot(current_cycle,post_mse_norm,'b*',current_cycle,post_upper,'r^');
 ylim([0,Inf]);
 %line([current_cycle,current_cycle],[mse_norm+cifac*stderr,mse_norm-cifac*stderr]);
 if first==false
-    times = [current_cycle-printcycle, current_cycle];
-    old_prior_upper = old_prior_mse_norm + cifac*old_prior_stderr;
-    if current_cycle >= da_spinup + printcycle
-        plot(times,[old_prior_mse_norm,prior_mse_norm],'m-.',...
-            times,[old_prior_upper,prior_upper],'m:');
-    end
+    times = [old_cycle, current_cycle];
+%     old_prior_upper = old_prior_mse_norm + cifac*old_prior_stderr;
+%     if current_cycle >= da_spinup + printcycle
+%         plot(times,[old_prior_mse_norm,prior_mse_norm],'m-.',...
+%             times,[old_prior_upper,prior_upper],'m:');
+%     end
     old_post_upper = old_post_mse_norm + cifac*old_post_stderr;
     plot(times,[old_post_mse_norm,post_mse_norm],'b-',...
         times,[old_post_upper,post_upper],'r--');
@@ -54,9 +55,11 @@ ttl1=sprintf('K=%d: Normalized Posterior Mean Squared Error Estimate',K);
 ttl2=sprintf('%8.6f + %8.6f (%4.1f%% 1-Sided CI)',...
     post_mse_norm,cifac*post_stderr,100*ci);
 title({ttl1,ttl2});
-old_prior_mse_norm = prior_mse_norm;
-old_prior_stderr = prior_stderr;
+legend('Posterior MSE', '1-Sided CI')
+% old_prior_mse_norm = prior_mse_norm;
+% old_prior_stderr = prior_stderr;
 old_post_mse_norm = post_mse_norm;
 old_post_stderr = post_stderr;
+old_cycle = current_cycle;
 drawnow
 end 
